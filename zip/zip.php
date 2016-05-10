18388bb651a0a12758b2c527bd988ff9c0f2eb12
jijiaogala <1249929810@qq.com>
2016-05-10 11:53:41 +0800

    php压缩文件案例

--- zip/zip.php
+++ zip/zip.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
+ * php添加文件到压缩包
  * User: admi
  * Date: 2016/5/9
  * Time: 12:02
@@ -8,7 +8,7 @@
 
 $zip = new ZipArchive();
 $filename = "./test.zip";
-$thisdir = '.';
+$thisDir = '.';
 
 if ($zip->open($filename, ZIPARCHIVE::CREATE) !== TRUE) {
     exit("cannot open <$filename>\n");
@@ -16,8 +16,8 @@ if ($zip->open($filename, ZIPARCHIVE::CREATE) !== TRUE) {
 
 $zip->addFromString("testfilephp.txt", "#1 This is a test string added as testfilephp.txt.\n");
 $zip->addFromString("testfilephp2.txt", "#2 This is a test string added as testfilephp2.txt.\n");
-$zip->addFile($thisdir . "/1.png", "1.png");
-$zip->addFile($thisdir . "/a.txt", "a.txt");
+$zip->addFile($thisDir . "/1.png", "1.png");
+$zip->addFile($thisDir . "/a.txt", "a.txt");
 echo "numfiles: " . $zip->numFiles . "\n";
 echo "status:" . $zip->status . "\n";
 $zip->close();
