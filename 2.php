5d738f57c97c56133ee95f8695334b607b716627
jijiaogala <1249929810@qq.com>
2016-12-05 10:47:10 +0800

    分割数组

--- 2.php
+++ 2.php
@@ -8,3 +8,7 @@ $tmpArray = array(1,2,3,4,5);
 $a = array_chunk($tmpArray, 2);
 var_dump($a);
 
+//一个数组当键值，一个数组当值
+$b = array('a', 'b', 'c', 'd', 'e');
+$c = array_combine($tmpArray, $b);
+var_dump($c);
